3bbd217df585918927d5f032647257df75e59d3b
Macbook <macbook@MacBook-de-Macbook.local>
2021-02-14 23:12:01 -0300

    Inseri o exercício dois

--- src/br/com/daiana/Main.java
+++ src/br/com/daiana/Main.java
@@ -11,26 +11,27 @@ public class Main {
 	// write your code here
 
         Scanner entrada = new Scanner(System.in);
-
-     //   Date dataDeNascimento = new Date();
-        //12/03/1985 - 26/09/2020
-        //3456789765 - 3456789454 /1000/60/24
-        //2345678654324564322
-
-        int numeroUm;
-        int numeroDois;
-        int resultado;
-
-        List<Integer> numeros = new ArrayList<>();
-
-        System.out.print("Digite o primeiro número:  ");
-        numeroUm = entrada.nextInt();
-
-        System.out.print("Digite o segundo número:  ");
-        numeroDois = entrada.nextInt();
-
-        resultado = numeroUm + numeroDois;
-        System.out.println("Resultado   "+ resultado);
+        Date datadeNascimento = new Date();
+//
+//     //   Date dataDeNascimento = new Date();
+//        //12/03/1985 - 26/09/2020
+//        //3456789765 - 3456789454 /1000/60/24
+//        //2345678654324564322
+//
+//        int numeroUm;
+//        int numeroDois;
+//        int resultado;
+//
+//        List<Integer> numeros = new ArrayList<>();
+//
+//        System.out.print("Digite o primeiro número:  ");
+//        numeroUm = entrada.nextInt();
+//
+//        System.out.print("Digite o segundo número:  ");
+//        numeroDois = entrada.nextInt();
+//
+//        resultado = numeroUm + numeroDois;
+//        System.out.println("Resultado   "+ resultado);
 
 
 //        String lido = entrada.next();
@@ -43,6 +44,64 @@ public class Main {
 
 
      // Exercício 2:
+
+        int dataDeHoje;
+        int mesAtual;
+        int anoVigente;
+
+        int diaDoNascimento;
+        int mesDoNascimento;
+        int anoDoNascimento;
+        int totalDeDias;
+        int totalDeMeses;
+        int totalEmAnos;
+        final int TOTAL_DE_DIAS_NO_ANO = 360;
+        final int TOTAL_DE_DIAS_NO_MES = 30;
+        int resultado;
+
+
+        System.out.print("Digite o dia do seu nascimento:    ");
+        diaDoNascimento = entrada.nextInt();
+
+        System.out.print("Digite o mês do seu nascimento:    ");
+        mesDoNascimento = entrada.nextInt();
+
+        System.out.print("Digite o ano do seu nascimento:    ");
+        anoDoNascimento = entrada.nextInt();
+
+        System.out.print("Digie a data de hoje:    ");
+        dataDeHoje = entrada.nextInt();
+
+        System.out.print("Digite o mês atual:    ");
+        mesAtual = entrada.nextInt();
+
+        System.out.print("Digite o ano vigente:    ");
+        anoVigente = entrada.nextInt();
+
+        totalDeDias = dataDeHoje - diaDoNascimento;
+        totalDeMeses = mesAtual - mesDoNascimento;
+        totalEmAnos = anoVigente - anoDoNascimento;
+
+        resultado = (totalEmAnos * TOTAL_DE_DIAS_NO_ANO) + (totalDeMeses * TOTAL_DE_DIAS_NO_MES) + totalDeDias;
+        System.out.println("Você viveu:   " + resultado + " dias! ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     // Exercício 3:
